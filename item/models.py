d09f5a2600da0d8f9d904638874932267aec4dc4
Benjamin <hbenji@hotmail.com>
2024-03-18 14:28:12 +0100

    Add category model - add category model to administration

--- item/models.py
+++ item/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+
