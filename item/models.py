9070954caa00359614813b66c8ca8129f3333ae2
Benjamin <hbenji@hotmail.com>
2024-03-18 14:38:09 +0100

    Add item model and add to administration

--- item/models.py
+++ item/models.py
@@ -13,3 +13,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    create_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
