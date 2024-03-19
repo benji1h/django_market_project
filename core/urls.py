c4da1cb4c2e7729b3972264b371a53cff0875f50
Benjamin <hbenji@hotmail.com>
2024-03-19 14:32:42 +0100

    Update urls project and app

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+from core.views import contact
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact', contact, name='contact'),
+]
